808e2122f8054dfb587e8289027e63383cdaa666
joaomena17 <joao.17.2001@gmail.com>
2024-07-22 11:07:07 -0700

    test

--- src/test_turtle.py
+++ src/test_turtle.py
@@ -7,7 +7,7 @@ from rospy.exceptions import ROSInterruptException
 if __name__ == '__main__':
     try:
         turtle_square = TurtleSquare()
-        turtle_square.move()
+        turtle_square.move(speed=1, length=5)
 
     except ROSInterruptException:
         print("Interrupting execution...")
